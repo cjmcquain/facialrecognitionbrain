a8a1644f5203a713345a430c301f626874e6094f
Charles McQuain <charlesmcquain@gmail.com>
2018-02-07 23:30:36 -0800

    Added React-Particles-JS library and completed design of landing page

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,9 +8,9 @@ const ImageLinkForm = () => {
                 {'This Magic Brain will detect faces in your pictures. Give it a try!'}
             </p>
             <div className="center">
-                <div className="form br3 shadow-5">
-                    <input className="f4 pa2 w-70 center" type='text' />
-                    <button className="w-30 grow f4 link ph3 dib white bg-light">Detect</button>
+                <div className="form center pa4 br3 shadow-5">
+                    <input className="f4 pa2 w-70 center" type='tex' />
+                    <button className="w-30 grow f4 link ph3 dib white bg-light-purple">Detect</button>
                 </div>
             </div>
         </div>
